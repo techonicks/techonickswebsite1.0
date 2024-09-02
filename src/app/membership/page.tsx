bf48055ccf104e67e26da54a9fbd2a66c55e710f
Tech O Nicks <sayan321x@gmail.com>
2024-09-03 02:11:33 +0530

    form submit loader added

--- src/app/membership/page.tsx
+++ src/app/membership/page.tsx
@@ -1,43 +1,7 @@
-"use client";
-
-import FormSubmitLoader from "@/components/FormSubmitLoader";
-import JoinUsForm from "@/components/Join/JoinUsForm";
-import { Spotlight } from "@/components/ui/Spotlight";
-import  LoadingContextProvider  from "@/context/loadingContext";
-import { useState } from "react";
+import MembershipPage from "@/components/Join/MembershipPage";
 
 const page = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const handleToggleLoading = (loading:boolean): void => {
-    setIsLoading(loading);
-    console.log(isLoading);
-    
-  };
-
-  return (
-    <LoadingContextProvider value={{ isLoading, handleToggleLoading }}>
-        {
-          isLoading && <FormSubmitLoader/>
-        }
-      <div className={`flex flex-col sm:flex-row justify-center items-center gap-4 relative ${isLoading && 'opacity-40'}`}>
-        <Spotlight
-          className="-top-40 left-0 md:left-60 md:-top-20"
-          fill="white"
-        />
-        <div className="px-4 flex flex-col justify-around">
-          <h2 className="font-bold text-4xl text-neutral-200">
-            Become a Member Today!
-          </h2>
-          <p className="text-[1.2rem] mt-4 max-w-sm  text-neutral-300">
-            Embark on an exciting journey of learning, growth, and innovation
-            with us. Together, let us push the boundaries of technology and make
-            a lasting impact.
-          </p>
-        </div>
-        <JoinUsForm />
-      </div>
-    </LoadingContextProvider>
-  );
+  return <MembershipPage />;
 };
 
 export default page;
