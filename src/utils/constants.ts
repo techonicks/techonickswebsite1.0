604b9fc2604b64f0187107348beff88905fa25f3
SayanSutradhar1 <sayan321x@gmail.com>
2024-12-11 22:38:40 +0530

    small change

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // export const AUTH_BASE_URL : string = "http://localhost:9090/api/v1"
 // export const AUTH_BASE_URL : string = "https://techonicksbackend.vercel.app/api/v1"
-export const AUTH_BASE_URL : string = "https://techonickswebbackend1-0-git-dev-techonicks-projects.vercel.app/api/v1"
+export const AUTH_BASE_URL : string = "https://techonickswebbackend1-0-91zhsxmra-techonicks-projects.vercel.app/api/v1"
 export const DB_NAME = "TechONicksWeb"
