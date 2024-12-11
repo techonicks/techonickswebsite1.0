2315549118688a1d1b9599092a7ff7add60aec58
SayanSutradhar1 <sayan321x@gmail.com>
2024-12-11 22:31:37 +0530

    minor

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
 // export const AUTH_BASE_URL : string = "http://localhost:9090/api/v1"
 // export const AUTH_BASE_URL : string = "https://techonicksbackend.vercel.app/api/v1"
-export const AUTH_BASE_URL : string = "https://techonickswebbackend1-0-git-dev-techonicks-projects.vercel.app/"
+export const AUTH_BASE_URL : string = "https://techonickswebbackend1-0-git-dev-techonicks-projects.vercel.app/api/v1"
 export const DB_NAME = "TechONicksWeb"
