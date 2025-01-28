05b900d85aeef25e1f156ea07b5159c9e1dc9dc7
SayanSutradhar1 <sayan321x@gmail.com>
2025-01-28 16:59:07 +0530

    minor

--- src/lib/fetch/getMembers.ts
+++ src/lib/fetch/getMembers.ts
@@ -1,12 +1,9 @@
 import { UsersFetchResponse } from "@/interfaces/api.interface"
-import { MemberWithCredentials } from "@/interfaces/member.interface"
 import { AUTH_BASE_URL } from "@/utils/constants"
 import axios from "axios"
 
 export const getAllMembers = async()=>{
-    const response = await axios.post(AUTH_BASE_URL+"/user/members/getAllMembers",{},{
-        withCredentials:true
-    })
+    const response = await axios.post(AUTH_BASE_URL+"/user/members/getAllMembers")
     const data = response.data as UsersFetchResponse
     return data
 }
